366b2b114d93ac8322f79add28554792bff448bc
vikassinghv34 <72185764+vikassinghv34@users.noreply.github.com>
2020-12-09 11:54:46 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,87 @@
+<!-- <style>
+    .footer {
+        position: absolute;
+        /* left: 0;*/
+        bottom: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+
+    }
+
+    .footer p {
+        text-align: center;
+        color: dodgerblue;
+        font-size: 1rem;
+        height: 40px;
+        line-height: 1rem;
+        padding-top: 10px;
+        margin-bottom: 0;
+    }
+</style>
+<footer>
+    <p>©LifeCare 2020</p>
+</footer> -->
+
+<!-- 
+<style>
+        .footer {
+            position: absolute;
+            /* left: 0;*/
+            bottom: 0;
+            width: 100%;
+            background-color: #f1f1f1;
+            /* margin-top: auto; */
+
+        }</style> -->
+
+        <!-- .footer p {
+            text-align: center;
+            color: dodgerblue;
+            font-size: 1rem;
+            height: 40px;
+            line-height: 1.5rem;
+            padding-top: 10px;
+            margin-bottom: 0;
+        }
+
+
+    <div class="footer">
+        <p>©LifeCare 2020</p>
+    </div> -->
+
+    <style>
+        .footer {
+            /* position: absolute; */
+            left: 0;
+            display: flex;
+            justify-content: space-around;
+            z-index:10;
+            bottom: 0;
+            width: 100%;
+            margin-bottom: 0px;
+            left:0px;
+            overflow:hidden;
+            
+            /* height:10vh; */
+            /* background-color: #f1f1f1; */
+            background-color: black;
+            /* margin-top: auto; */
+            bottom: 0;
+        }
+
+        .footer p {
+            text-align: center;
+            color: dodgerblue;
+            font-size: 1rem;
+            height: 40px;
+            line-height: 1.5rem;
+            padding-top: 10px;
+            /* margin-bottom: 0;
+            margin: 0;
+            padding: 0; */
+        }
+    </style>
+
+    <div class="footer">
+        <p>©LifeCare 2020</p>
+    </div>
